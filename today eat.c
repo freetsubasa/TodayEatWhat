7a74dfd3cb3a61512000c9141ba2c7b2944e5aa3
freetsubasa <freetsubasa@gmail.com>
2014-07-18 02:39:57 +0800

    究竟今天小飛機想吃什麼呢？

---
+++ today eat.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+	while(1)
+	{
+		int t = (int) time (NULL);		
+		int food;
+
+		if( == "school")
+			{
+				int i = 0;
+				for(i = 0 ; i < 3 ; i++)
+					{
+						int a = 1;
+						food = t%3;
+						
+						switch(food)
+							{
+								case 0:
+									printf("%d. 二餐一樓煎餃\n",a);
+									break;
+								case 1:
+									printf("%d. 二餐便當\n",a);
+									break;
+								case 2:
+									printf("%d. 二餐三樓\n",a);
+									break;
+								case 3:
+									printf("%d. 女二\n",a)
+									break;
+							}
+						
+					}
+			}
+		else( == "out")
+			{
+				switch(food)
+			}
+	}
+}
