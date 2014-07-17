6e352e3a69c62b70a56b96ab5a6c868ef301ba86
freetsubasa <freetsubasa@gmail.com>
2014-07-18 03:23:39 +0800

    根本今天吃 for 小飛機版本

--- today eat.c
+++ today eat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
@@ -8,36 +9,94 @@ int main(int argc, char **argv)
 	{
 		int t = (int) time (NULL);		
 		int food;
-
-		if( == "school")
+		int ans;
+		srand(time(NULL));
+		
+		if (strcmp(argv[1], "school") == 0)
 			{
 				int i = 0;
 				for(i = 0 ; i < 3 ; i++)
 					{
-						int a = 1;
-						food = t%3;
-						
+						food = (rand() % 3) +1;
 						switch(food)
 							{
 								case 0:
-									printf("%d. 二餐一樓煎餃\n",a);
+									printf("%d. 二餐一樓煎餃\n",i+1);
 									break;
 								case 1:
-									printf("%d. 二餐便當\n",a);
+									printf("%d. 二餐便當\n",i+1);
 									break;
 								case 2:
-									printf("%d. 二餐三樓\n",a);
+									printf("%d. 二餐三樓\n",i+1);
 									break;
 								case 3:
-									printf("%d. 女二\n",a)
+									printf("%d. 女二\n",i+1);
 									break;
 							}
-						
 					}
 			}
-		else( == "out")
+		else if (strcmp(argv[1], "home") == 0)
 			{
-				switch(food)
+				int i = 0;
+
+				for (int i = 0; i < 3; ++i)
+				{
+					int b = 1;
+					food = (rand() % 7) +1;
+
+					switch(food)
+						{
+							case 0:
+							printf("%d. 清夜義大利麵\n",i+1);
+							break;
+
+							case 1:
+							printf("%d. 清夜二六食堂\n",i+1);
+							break;
+
+							case 2:
+							printf("%d. 清夜炒飯\n",i+1);
+							break;
+
+							case 3:
+							printf("%d. 清夜鐵板燒\n",i+1);
+							break;
+
+							case 4:
+							printf("%d. 清夜我家小館\n",i+1);
+							break;
+
+							case 5:
+							printf("%d. 便當店\n",i+1);
+							break;
+
+							case 6:
+							printf("%d. 漢堡\n",i+1);
+							break;
+
+							case 7:
+							printf("%d. pizza\n",i+1);
+							break;			
+						}
+				}
 			}
+
+		printf("小飛機你今天想吃什麼 1、2、3 or ... me ?\n");
+		while(scanf("%d", &ans) == 1)
+		{
+			if (ans == 1 || ans == 2 || ans == 3)
+			{
+				printf("那就快點去吃吧 <3\n");
+			}
+			else if (ans == 4)
+			{
+				printf("居然打槍我 QQ\n");
+			}
+			else
+			{
+				printf("輸入其他數字是想吃掉趴趴子的意思嘛 >////<\n");
+			}
+		}
 	}
 }
+
