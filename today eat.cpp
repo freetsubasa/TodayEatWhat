48e19eb2a7c8172ffde6af13cf2cdd88e08d6b56
Roan Huang <pichuang@cs.nctu.edu.tw>
2014-07-26 19:09:35 +0800

    clean code

--- today eat.cpp
+++ today eat.cpp
@@ -13,81 +13,78 @@ int main(int argc, char **argv)
 		srand(time(NULL));
 		
 		if (strcmp(argv[1], "school") == 0)
+		{
+			int i = 0;
+			for(i = 0 ; i < 3 ; i++)
 			{
-				int i = 0;
-				for(i = 0 ; i < 3 ; i++)
-					{
-						food = (rand() % 3) +1;
-						switch(food)
-							{
-								case 0:
-									printf("%d. 二餐一樓煎餃\n",i+1);
-									break;
-								case 1:
-									printf("%d. 二餐便當\n",i+1);
-									break;
-								case 2:
-									printf("%d. 二餐三樓\n",i+1);
-									break;
-								case 3:
-									printf("%d. 女二\n",i+1);
-									break;
-							}
-					}
-			}
-		else if (strcmp(argv[1], "home") == 0)
-			{
-				int i = 0;
-
-				for (int i = 0; i < 3; ++i)
+				food = (rand() % 3) +1;
+				switch(food)
 				{
-					int b = 1;
-					food = (rand() % 7) +1;
-
-					switch(food)
-						{
-							case 0:
-							printf("%d. 清夜義大利麵\n",i+1);
-							break;
-
-							case 1:
-							printf("%d. 清夜二六食堂\n",i+1);
-							break;
-
-							case 2:
-							printf("%d. 清夜炒飯\n",i+1);
-							break;
-
-							case 3:
-							printf("%d. 清夜鐵板燒\n",i+1);
-							break;
-
-							case 4:
-							printf("%d. 清夜我家小館\n",i+1);
-							break;
-
-							case 5:
-							printf("%d. 便當店\n",i+1);
-							break;
-
-							case 6:
-							printf("%d. 漢堡\n",i+1);
-							break;
-
-							case 7:
-							printf("%d. pizza\n",i+1);
-							break;			
-						}
+					case 0:
+						printf("%d. 二餐一樓煎餃\n",i+1);
+						break;
+					case 1:
+						printf("%d. 二餐便當\n",i+1);
+						break;
+					case 2:
+						printf("%d. 二餐三樓\n",i+1);
+						break;
+					case 3:
+						printf("%d. 女二\n",i+1);
+						break;
 				}
 			}
-		else if (strcmp(argv[1], "info") == 0)
+		}
+		else if (strcmp(argv[1], "home") == 0)
+        {
+			for (int i = 0; i < 3; ++i)
 			{
-				printf("home => 吃除了學校以外的東西\n");
-				printf("school => 學校裡的東西\n");
-				printf("然後 info 就相關資訊 ._.\n");
-				printf("https://github.com/freetsubasa/TodayEatWhat 求改良（？）\n");
-				break;
+				food = (rand() % 7) +1;
+
+				switch(food)
+				{
+					case 0:
+					printf("%d. 清夜義大利麵\n",i+1);
+					break;
+
+					case 1:
+					printf("%d. 清夜二六食堂\n",i+1);
+					break;
+
+					case 2:
+					printf("%d. 清夜炒飯\n",i+1);
+					break;
+
+					case 3:
+					printf("%d. 清夜鐵板燒\n",i+1);
+					break;
+
+					case 4:
+					printf("%d. 清夜我家小館\n",i+1);
+					break;
+
+					case 5:
+					printf("%d. 便當店\n",i+1);
+					break;
+
+					case 6:
+					printf("%d. 漢堡\n",i+1);
+					break;
+
+					case 7:
+					printf("%d. pizza\n",i+1);
+					break;			
+				}
 			}
+		}
+		else if (strcmp(argv[1], "info") == 0)
+		{
+			printf("home => 吃除了學校以外的東西\n");
+			printf("school => 學校裡的東西\n");
+			printf("然後 info 就相關資訊 ._.\n");
+			printf("https://github.com/freetsubasa/TodayEatWhat 求改良（？）\n");
+			break;
+		}
 		printf("小飛機你今天想吃什麼 1、2、3 or ... me ?\n");
 		while(scanf("%d", &ans) == 1)
 		{
